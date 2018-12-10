b3445b1faf6ed8f75854b5f24ef9d505e81b6cde
Jazzonz <jasonwong640@gmail.com>
2018-12-10 21:22:29 +0800

    FlexboxScreen

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import {Button, Avatar} from 'react-native-elements';
 import Header from './Components/header';
 import {createStackNavigator, createAppContainer} from 'react-navigation'
 import Todo from './Screen/Todo';
+import Flexbox from './Screen/Flexbox';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
       <Header textIwant={"hello"} />
+      <View style={{ padding: 20, backgroundColor: "blue" }} />
       <Header textIwant={"hello2"} />
+      <View style={{ padding: 20 }} />
       <Button title={"HI"} 
       onPress={()=>{ console.log('hello'); }}
       color="#deb887"
-     ></Button>
+      />
         <Text style={styles.bigblue} >Click the button!</Text>
         <Avatar
   size="large"
@@ -22,33 +25,41 @@ export default class App extends React.Component {
   onPress={() => console.log("Works!")}
   activeOpacity={0.7}
 />
-title= "Go to Todo"
-onPress={() => this.props.navigation.navigate('TodoScreen')}
-      </View>
+<Button
+ title= "Go to Todo"
+onPress={() => this.props.navigation.navigate("TodoScreen")} 
+/>
+<Button
+title= "Go to Flexbox"
+onPress={() => this.props.navigation.navigate("FlexboxScreen")}
+/>
+      </View> 
     );
   }
 }
 
 const AppNavigator = createStackNavigator({
   Home: {
-    screen: App,
+    screen: App },
     TodoScreen: Todo,
-  }
-});
+    FlexboxScreen: Flexbox
+  });
 
 const AppContainer = createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#ffebcd',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   bigblue: {
     color: "blue",
     fontWeight: 'bold',
   }
 });
-export default AppNavigator
-export default AppContainer
+
+export default AppContainer;
--- Screen/Todo.js
+++ Screen/Todo.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Todo extends React.Component{
     render(){
-        return (<Text>Todo App</Text>)
+        return (
+            <View style ={{flex: 1, flexDirection: 'column'}}>
+            <View style = {{ width: 50, height: 50, backgroundColor: "blue" }} />
+            <View style = {{ width: 100, height: 100, backgroundColor: "pink" }} />
+            <View style = {{ width: 150, height: 150, backgroundColor: "red" }} />
+            </View>
+        ) 
     }
 }
 
-export default TodoScreen;
+export default Todo;
