c3314aace221c2980e4ea45dd4f5b77e17212732
Jazzonz <jasonwong640@gmail.com>
2018-12-11 21:56:49 +0800

    Section header component added

--- Screen/Todo.js
+++ Screen/Todo.js
@@ -1,16 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList } from 'react-native';
 
 class Todo extends React.Component{
+
+    renderRow = ({ item }) =>
+    <View style={styles.row}>
+    <Text>{item.text}</Text>
+    </View>;
+
+    renderSectionHeader = ({ section }) =>
+    <View style ={styles.header}>
+    <Text>{section.title}</Text>
+    </View>;
+
+
     render(){
         return (
-            <View style ={{flex: 1, flexDirection: 'column'}}>
-            <View style = {{ width: 50, height: 50, backgroundColor: "blue" }} />
-            <View style = {{ width: 100, height: 100, backgroundColor: "pink" }} />
-            <View style = {{ width: 150, height: 150, backgroundColor: "red" }} />
-            </View>
-        ) 
-    }
-}
+            <SectionList
+         sections = {sections}
+         renderItem={this.renderflow}
+         renderSectionHeader={this.renderSectionHeader}
+      
+/>
+        );
+        }
 
+        const sections = [ 
+            id: 0,
+            title: 'Section 1',
+            data:
+            [
+                {id: 0, text: 'Item 1'},
+                {id: 1, text: 'Item 2'}
+            ]
+        
+        ]
+    },
+
+    {id: 1, title: 'Section 2', data: [{id: 3, text: 'Item 4'}, {id: 4, text: 'Item'}]}
+
+
+
+const styles = StyleSheet.create({
+    row: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'skyblue',
+    },
+     header: {
+         padding: 15,
+         marginBottom: 5,
+backgroundColor: 'black',
+     }
+    }
+   
 export default Todo;
